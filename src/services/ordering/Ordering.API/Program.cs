1ccc57604e44e7fd338ecdea13d1b3790f4ea1c5
Manoj Garg <getgarged@gmail.com>
2022-05-24 17:10:16 +0530

    Rabbit MQ Event Generation and Consumption

--- src/services/ordering/Ordering.API/Program.cs
+++ src/services/ordering/Ordering.API/Program.cs
@@ -1,3 +1,6 @@
+using EventBus.Messages.Common;
+using MassTransit;
+using Ordering.API.EventBusConsumer;
 using Ordering.API.Extensions;
 using Ordering.Application;
 using Ordering.Infrastructure;
@@ -10,9 +13,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+
+// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config => {
+
+    config.AddConsumer<BasketCheckoutConsumer>();
+
+    config.UsingRabbitMq((ctx, cfg) => {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+        cfg.UseHealthCheck(ctx);
+
+        cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c => {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
+
+// General Configuration
+builder.Services.AddScoped<BasketCheckoutConsumer>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
